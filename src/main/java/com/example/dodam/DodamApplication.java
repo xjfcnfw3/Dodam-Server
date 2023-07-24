290e5ec340ae3aa37ad794f14b072db5688396b3
kimminsu <xjfcnfw3@naver.com>
2023-07-24 20:10:09 +0900

    refactor: Change passwordEncoder to declare in config class

--- src/main/java/com/example/dodam/DodamApplication.java
+++ src/main/java/com/example/dodam/DodamApplication.java
@@ -1,24 +1,15 @@
 package com.example.dodam;
 import com.example.dodam.config.auth.AuthProperties;
-import java.sql.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AuthProperties.class)
 public class DodamApplication {
 
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
 	public static void main(String[] args) {
-		SpringApplication.run(DodamApplication.class, args);  //스프링 부트 프로젝트의 시작
+		SpringApplication.run(DodamApplication.class, args);
 	}
 }
 
--- src/main/java/com/example/dodam/config/SecurityConfig.java
+++ src/main/java/com/example/dodam/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -28,6 +29,10 @@ public class SecurityConfig {
 		return new JwtFilter(tokenProvider, memberDetailsService);
 	}
 
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	@Bean
 	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
